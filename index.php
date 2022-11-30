99242dde48e451ee03d09a4bcf06175272b0a45a
MirkoFarina <MirkoFarina@outlook.com>
2022-11-30 15:29:44 +0100

    first commit whit text area

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <form action="./landing-page.php" method="POST">
+        <label for="text">
+            INSERISCI QUI IL TESTO <br>
+        </label>
+        <textarea name="text" id="" cols="30" rows="5" placeholder="INSERISCI IL TESTO DA INVIARE ...">
+            
+        </textarea>
+        <button type="submit">
+            INVIA
+        </button>
+    </form>
+</body>
+</html>
